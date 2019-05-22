3aadb3f5bd848565aa2567b32fe4e035be675dd3
asingh <singajeet@gmail.com>
2019-05-23 01:39:43 +0530

    Initial commit again

---
+++ core/db/categories.py
@@ -0,0 +1,55 @@
+"""This module lists all the database types as classes that are supported
+    by the system. Each class will be used to identify the plugins to be
+    loaded based on the db type configured.
+
+    Author: Ajeet Singh
+    Date: 5/22/2019
+"""
+from yapsy.IPlugin import IPlugin
+
+
+
+class AbstractCategory(IPlugin):
+    """ An abstract category
+    """
+    name = "AbstractCategory"
+
+    def db_type(self):
+        """Returns database type
+        """
+        return self.name
+
+    def used_by(self):
+        """Returns the usage of this plugin i.e., used by Connection
+            Manager, Security Manager, etc
+        """
+        pass
+
+    def __repr__(self):
+        """An string representation
+        """
+        return self.name
+
+
+class SQLite(AbstractCategory):
+    """ An SQLite db type as category
+    """
+    name = "SQLITE"
+
+
+class MariaDB(AbstractCategory):
+    """ An MariaDB type as category
+    """
+    name = "MARIADB"
+
+
+class MySQL(AbstractCategory):
+    """ An MySQL db type category
+    """
+    name = "MYSQL"
+
+
+class Oracle(AbstractCategory):
+    """ An Oracle DB type as category
+    """
+    name = "ORACLE"
--- core/db/connection_managers/sqlite.py
+++ core/db/connection_managers/sqlite.py
@@ -5,10 +5,11 @@
 """
 from sqlalchemy import create_engine, inspect
 from sqlalchemy.orm import sessionmaker
+import bipy.core.db.categories as categories
 
 
-class ConnectionManager:
-    """ A SQLite connection manager class which is a
+class ConnectionManager(categories.SQLite):
+    """ A SQLite connection manager which is a
         singleton and should return same instance always
     """
 
@@ -19,37 +20,38 @@ class ConnectionManager:
     Session = None
     __instance = None
 
-    def __new__(cls, val):
-        """ The singleton constructor for this class
+    """def __new__(cls, val):
+         The singleton constructor for this
 
             Args:
                 cls (object): cls
                 val (object): obj
 
             Return:
-                ConnectionManager: An singleton instance of this class
-        """
+                ConnectionManager: An singleton instance of this
+        
         if ConnectionManager.__instance is None:
             ConnectionManager.__instance = object.__new__(cls)
         ConnectionManager.__instance.val = val
         return ConnectionManager.__instance
 
-    def __init__(self, conn_string):
-        """ Default consttructor of the Connection Manager class
+     def __init__(self, conn_string):
+         Default consttructor of the Connection Manager class
 
             Args:
                 conn_string (String): The connection string to be used by
                                         database engine for making connection
-        """
+        
         self.connection_string = conn_string
         self.engine = create_engine(conn_string)
         self.Session = sessionmaker()
         self.Session.configure(bind=self.engine)
         self.ConnectedSession = self.Session()
         self.inspector = inspect(self.engine)
+    """
 
     def get_connection_string(self):
-        """Returns the connection string used in this class for mmaking
+        """Returns the connection string used in this for mmaking
             connections to the database
         """
         return self.connection_string
