3b6a68ce08d6fe0991d064cc74cecea7a64e5577
singajeet <singajeet@gmail.com>
2019-06-09 22:22:06 +0530

    Added config manager

--- bipy/core/config/manager.py
+++ bipy/core/config/manager.py
@@ -3,5 +3,65 @@
     Author: Ajeet Singh
     Date: 06/09/2019
 """
-import config
+from config import Config
+import os
 from bipy.core.constants import PATHS
+from bipy.logging import logger
+from bipy.core import constants
+
+LOGGER = logger.get_logger(__name__)
+
+
+class ConfigManager:
+    """ Singleton class to work with comfiguration
+    by whole system
+    """
+
+    __INSTANCE = None
+    __config_dir = None
+    __config_file = None
+    CONFIG = None
+
+    def __new__(cls):
+        """Method overwrite to implement singleton
+        class
+        """
+        if ConfigManager.__INSTANCE is None:
+            ConfigManager.__INSTANCE = object.__new__(cls)
+        return ConfigManager.__INSTANCE
+
+    def __init__(self):
+        """ Default conatructor """
+        LOGGER.debug("ConfigManaqer instance created")
+        self.__config_dir = PATHS.CONFIG
+        self.__config_file = os.path.abspath(
+            os.path.join(self.__config_dir, "default.cfg"))
+        _cf = open(self.__config_file)
+        self.CONFIG = Config(_cf)
+        self.CONFIG.addNamespace(constants)
+        LOGGER.debug("Config file opened available at '%s'" %
+                     self.__config_file)
+
+    def set_location(self, new_path):
+        """Sets a new path of config file instead of using
+        default"""
+        self.__config_file = os.path.abspath(new_path)
+        self.__config_dir = os.path.abspath(os.path.
+                                            dirname(self.__config_file))
+        _cf = open(self.__config_file)
+        self.CONFIG = Config(_cf)
+        self.CONFIG.addNamespace(constants)
+        LOGGER.debug("New location set of config file at: '%s'" %
+                     self.__config_file)
+
+    def get_file_location(self):
+        """Returns location of config file"""
+        return self.__config_file
+
+    def get_dir_location(self):
+        """Returns dir of config file """
+        return self.__config_dir
+
+    def get_config(self):
+        """ Returns config instance """
+        return self.CONFIG
